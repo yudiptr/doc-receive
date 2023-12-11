6f38cc6a213d7ba07fa2909cb460600daa9efa5e
yudiptr <yudiputrasabri789@gmail.com>
2023-12-11 12:29:21 +0700

    remove unused import

--- src/main/java/com/ddr/penerimaandocument/config/WebConfiguration.java
+++ src/main/java/com/ddr/penerimaandocument/config/WebConfiguration.java
@@ -1,16 +1,9 @@
 package com.ddr.penerimaandocument.config;
 
-
-// import org.apache.hc.core5.http.HttpClient;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
