27d0370dd84a0a6ea20cf1f0c5c6896d7c4b7efe
yudiptr <yudiputrasabri789@gmail.com>
2024-01-04 15:51:11 +0700

    remove unused imports

--- src/main/java/com/ddr/penerimaandocument/service/GeneratePDFService.java
+++ src/main/java/com/ddr/penerimaandocument/service/GeneratePDFService.java
@@ -2,7 +2,6 @@ package com.ddr.penerimaandocument.service;
 
 import com.lowagie.text.DocumentException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
